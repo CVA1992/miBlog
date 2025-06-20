e338549be45f4dfdb7b2b6d0a03be5a9d0b84c35
carlos <cvelasco6@umsa.bo>
2025-06-19 20:47:40 -0400

    add vista publicacion con comentarios

--- posts/urls.py
+++ posts/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from . import views
 
+app_name= 'posts'
+urlpatterns = [
+    path('publicacion/<int:pk>', views.publicacion, name="publicacion")
+]
 
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 
 # Create your views here.
+def publicacion(request, pk):
+    publicacion = get_object_or_404(Post, pk=pk)
+    comentarios = publicacion.comments.filter(active=True).order_by('-created')
+    contexto= {
+        'publicacion': publicacion,
+        'comentarios': comentarios
+    }
+    return render(request,'posts/publicacion.html',contexto)

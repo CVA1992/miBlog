a79986ba904159c681cafe7816c165c74d69d5dd
carlos <cvelasco6@umsa.bo>
2025-06-22 20:18:25 -0400

    add funcionalidad de editar publicacion

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name= 'posts'
 urlpatterns = [
     path('publicacion/<int:pk>', views.publicacion, name="publicacion"),
-    path('post/new/', views.crear_publicacion,name='post_form')
+    path('post/new/', views.crear_publicacion,name='post_form'),
+    path('publicacion/<int:pk>/editar/',views.publicacion_editar,name='publicacion_editar' )
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Post, Comment
 from .forms import PostForm
 
@@ -6,9 +8,11 @@ from .forms import PostForm
 def publicacion(request, pk):
     publicacion = get_object_or_404(Post, pk=pk)
     comentarios = publicacion.comments.filter(active=True).order_by('-created')
+
     contexto= {
         'publicacion': publicacion,
-        'comentarios': comentarios
+        'comentarios': comentarios,
+        'es_autor': request.user== publicacion.author
     }
     return render(request,'posts/publicacion.html',contexto)
 def crear_publicacion(request):
@@ -18,7 +22,25 @@ def crear_publicacion(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('posts:publicacion', pk=post.pk)
+            return redirect('posts:pu{% if es_autor %}blicacion', pk=post.pk)
     else:
         form=PostForm()
     return render(request,'posts/post_form.html',{'form':form})
+@login_required
+def publicacion_editar(request,pk):
+    publicacion = get_object_or_404(Post, pk=pk)
+
+    if request.user != publicacion.author:
+        messages.error(request,'no tienes permiso para editar esta publicacion')
+        return redirect('posts:publicacion', pk=publicacion.pk)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=publicacion)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Publicacion actualizada correctamente')
+            return redirect('posts:publicacion', pk=publicacion.pk)
+    else:
+        form = PostForm(instance=publicacion)
+    return render(request,'posts/publicacion_editar.html',{'form':form, 'publicacion':publicacion})
